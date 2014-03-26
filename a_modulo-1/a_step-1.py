440f040f8fa647396af4ff8a8de19c373c01cbf3
ThunderSoft <gustavo.adorno@gmail.com>
2014-03-26 10:40:45 -0300

    Update a_step-1.py

--- a_modulo-1/a_step-1.py
+++ a_modulo-1/a_step-1.py
@@ -20,11 +20,17 @@ fout.close()
 
 # Llamada a los scripts 
 
+# Busquedad en el log de todos los valores ##
 subprocess.call(['/DATA/a_modulo-2/a_step-2'])
+# Limpieza de los datos recolectados en el script anterior buscando usuario y los valores de ##r ##a etc
 subprocess.call(['/DATA/a_modulo-3/a_step-3'])
+# Ejecucion de los scripts de acuerdo a las ordenes obtenidas
 subprocess.call(['/DATA/a_modulo-4/a_step-4'])
+# Modulo que busca en la carpeta SHARED archivos con notificaciones a usuarios 
 subprocess.call(['/DATA/a_modulo-6/a_step-6'])
+# Modulo que genera la sesiones toma los valores de screen y conecta las unidades compartidas
 subprocess.call(['/DATA/a_modulo-8/a_step-8'])
+# Modulo de log guarda la infornacion en bruto en el archivo screen-history
 subprocess.call(['/DATA/a_modulo-9/a_step-9'])
 
 
